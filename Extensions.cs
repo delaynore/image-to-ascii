a22505d4158f70aa7ca6d4bf6f5d3807f2bc0af4
DanyaLozhkin <delaynore@gmail.com>
2023-12-15 12:09:24 +0400

    fix: change formula for pixel rgb to grayscale convertion

--- Extensions.cs
+++ Extensions.cs
@@ -12,8 +12,7 @@ namespace AsciiImage.Extensions
                 for (int x = 0; x < bitmap.Width; x++)
                 {
                     var pixel = bitmap.GetPixel(x, y);
-                    var avg = (pixel.R + pixel.G + pixel.B) / 3;
-                    var color = Color.FromArgb(pixel.A, avg, avg, avg);
+                    var color = Rgb2Gray(pixel);
                     grayscale.SetPixel(x, y, color);
                 }
             }
@@ -25,5 +24,10 @@ namespace AsciiImage.Extensions
             return new Bitmap(bitmap, size);
         }
 
+        public static Color Rgb2Gray(Color rgb)
+        {
+            var gray = Convert.ToInt32(0.299 * rgb.R + 0.587 * rgb.G + 0.114 * rgb.B);
+            return Color.FromArgb(rgb.A, gray, gray, gray);
+        }
     }
 }
